cecbfe4074b31b19ab0af369fc5ff560aee5af29
saloni pattnaik <salonipattnaik4@gmail.com>
2019-09-10 16:42:10 +0530

    AssPython Modification

---
+++ AssPython.py
@@ -0,0 +1,13 @@
+s="python for life"
+print(s)
+s1=s.split(" ")
+w=s1[0]
+w2=s1[1]
+w3=s1[2]
+print(s1[0])
+print(s1[1])
+print(s1[2])
+print(s[2:10])
+print(s[8:13])
+print(w, w2, w3.upper())
+print(w.upper(), w2, w3.upper())
