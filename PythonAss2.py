53689ac2a9823edea7003b412b85207a835c87c3
saloni pattnaik <salonipattnaik4@gmail.com>
2019-09-10 17:53:12 +0530

    Ass2

---
+++ PythonAss2.py
@@ -0,0 +1,23 @@
+a = 0
+b=10
+for num in range(a, b+1, 1):print(num, end=" ")
+print(" ")
+c=5
+d=25
+for num in range(c, d+1, 1):print(num, end=" ")
+print(" ")
+e=5
+f=25
+for num in range(e, f+1, 5):print(num, end=" ")
+print(" ")
+g=0
+h=25
+for num in range(h, g-1, -5):print(num, end=" ")
+print(" ")
+x=2
+y=100
+for num in range(x, y+1, 2):print(num, end=" ")
+print(" ")
+x1=1
+y2=100
+for num in range(x1, y2+1, 2):print(num, end=" ")
